663b72ef1031409f74359cbd9708a0725b30ee2f
I318869 <lyle.wang@sap.com>
2018-04-03 10:17:54 +0800

    add test

--- manage.py
+++ manage.py
@@ -1,4 +1,17 @@
 # -*- coding=utf-8 -*-
+import os
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                format='%(asctime)s %(filename)s [line:%(lineno)d] %(levelname)s %(message)s',
+                datefmt='%Y-%m-%d %H:%M:%S')
+
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='application/*')
+    COV.start()
+
 
 from flask_script import Manager, Server, Shell
 
@@ -12,6 +25,30 @@ def make_shell_context():
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('runserver', Server(host='127.0.0.1', port=5000))
+
+@manager.command
+def test(coverage=False):
+    """
+    docstring here
+        :param coverage=False: 
+    """
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        logging.info('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        logging.info(f'HTML location: file://{covdir}/index.html')
+        COV.erase()
 
 if __name__ == "__main__":
     manager.run()
